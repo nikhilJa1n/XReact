f364cce213a5ded5bf225a9932f3157eb3a87425
nikhilja1n <nikhilj50@gmail.com>
2019-07-26 11:19:51 +0530

    added text element and attributes support

--- src/XReact.js
+++ src/XReact.js
@@ -1,33 +1,111 @@
-// const { render } = require('./index.html');
-const XReact = importRender();
-const element = {
-  type: 'span',
-  props: {
-    children: [
-      {
-        type: 'TEXT ELEMENT',
-        props: { textValue: 'XReact' },
-      },
-    ],
-  },
-};
+const element = createElement('div', {}, createElement('p', {}, 'paraTry'));
 
-function importRender() {
-  function render(element, mainDom) {
+class XReact {
+  render(element, mainDom) {
     const { type, props } = element;
+    // console.log(element);
 
-    const isTextElement = type === 'TEXT ELEMENT';
+    const isTextElement = type === 'TEXT_ELEMENT';
     const dom = isTextElement
       ? document.createTextNode(props.textValue)
       : document.createElement(type);
+    // console.log(dom);
+
+    const isListener = name => name.startsWith('on');
+    Object.keys(props)
+      .filter(isListener)
+      .forEach(name => {
+        const eventType = name.toLowerCase().substring(2);
+        dom.addEventListener(eventType, props[name]);
+      });
+
+    // Set properties
+    const isAttribute = name => !isListener(name) && name != 'children';
+    Object.keys(props)
+      .filter(isAttribute)
+      .forEach(name => {
+        dom[name] = props[name];
+      });
 
     const childElements = props.children || [];
-    childElements.forEach(childElement => render(childElement, dom));
+    childElements.forEach(childElement => this.render(childElement, dom));
     mainDom.appendChild(dom);
   }
-  return {
-    render,
-  };
 }
 
-XReact.render(element, document.getElementById('root'));
+let React = new XReact();
+React.render(element, document.getElementById('root'));
+const element = createElement('div', {}, createElement('p', {}, 'paraTry'));
+
+class XReact {
+  render(element, mainDom) {
+    const { type, props } = element;
+    // console.log(element);
+
+    const isTextElement = type === 'TEXT_ELEMENT';
+    const dom = isTextElement
+      ? document.createTextNode(props.textValue)
+      : document.createElement(type);
+    // console.log(dom);
+
+    const isListener = name => name.startsWith('on');
+    Object.keys(props)
+      .filter(isListener)
+      .forEach(name => {
+        const eventType = name.toLowerCase().substring(2);
+        dom.addEventListener(eventType, props[name]);
+      });
+
+    // Set properties
+    const isAttribute = name => !isListener(name) && name != 'children';
+    Object.keys(props)
+      .filter(isAttribute)
+      .forEach(name => {
+        dom[name] = props[name];
+      });
+
+    const childElements = props.children || [];
+    childElements.forEach(childElement => this.render(childElement, dom));
+    mainDom.appendChild(dom);
+  }
+}
+
+let React = new XReact();
+React.render(element, document.getElementById('root'));
+const element = createElement('div', {}, createElement('p', {}, 'paraTry'));
+
+class XReact {
+  render(element, mainDom) {
+    const { type, props } = element;
+    // console.log(element);
+
+    const isTextElement = type === 'TEXT_ELEMENT';
+    const dom = isTextElement
+      ? document.createTextNode(props.textValue)
+      : document.createElement(type);
+    // console.log(dom);
+
+    const isListener = name => name.startsWith('on');
+    Object.keys(props)
+      .filter(isListener)
+      .forEach(name => {
+        const eventType = name.toLowerCase().substring(2);
+        dom.addEventListener(eventType, props[name]);
+      });
+
+    // Set properties
+    const isAttribute = name => !isListener(name) && name != 'children';
+    Object.keys(props)
+      .filter(isAttribute)
+      .forEach(name => {
+        dom[name] = props[name];
+      });
+
+    const childElements = props.children || [];
+    childElements.forEach(childElement => this.render(childElement, dom));
+    mainDom.appendChild(dom);
+  }
+}
+
+let React = new XReact();
+React.render(element, document.getElementById('root'));
