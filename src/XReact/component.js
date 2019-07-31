4a13fd03d15f59ef7d426f5baf8f67a7972e7d11
Karan Arora <karan.arora@mountblue.io>
2019-07-31 09:46:36 +0530

    Added Functionality for lifecycle methods(componentDidMount )

--- src/XReact/component.js
+++ src/XReact/component.js
@@ -1,14 +1,32 @@
+/* eslint-disable class-methods-use-this */
 /* eslint-disable import/extensions */
 import { renderAgain } from './render.js';
 
+const setStateCount = 0;
 export default class Component {
   constructor(props) {
     this.props = props;
     this.state = this.state || {};
+    // this.componentWillMount();
+    this.render();
+    setTimeout(() => {
+      this.componentDidMount();
+    }, 0);
+    // this.componentDidMount();
+  }
+
+  componentWillMount() {}
+
+  componentDidMount() {
+    // console.log('in Component Did mount');
   }
 
   setState(newState) {
+    console.log(newState);
     this.state = JSON.parse(JSON.stringify(newState));
+    this.render();
     renderAgain();
   }
+
+  render() {}
 }
--- test/component.test.js
+++ test/component.test.js
@@ -1,6 +1,6 @@
-import XReact from '../src/XReact.js';
-import Component from '../src/component.js';
-import { createElement } from '../src/createElement.js';
+import XReact from '../src/XReact/XReact.js';
+import Component from '../src/XReact/component.js';
+import createElement from '../src/XReact/createElement.js';
 
 export class App extends Component {
   constructor(props) {
@@ -14,13 +14,21 @@ export class App extends Component {
 
 describe('component render', () => {
   it('should render the components', async () => {
-    // const app = new App();
-    // console.log(app.render());
-    XReact.render('<App />', document.getElementById('root'));
+    const app = XReact.createElement(
+      App,
+      {
+        title: 'XReact',
+        author1: 'Karan Arora',
+        author2: 'Lokpati Mishra',
+        author3: 'Nikhil Jain',
+        onClick: () => alert('This is an onclick alert'),
+      },
+      null,
+    );
+
+    XReact.render(app, document.getElementById('root'));
     setTimeout(() => {
-      chai
-        .expect(document.getElementById('root').innerHTML)
-        .to.equal('<div><p>paraTry</p></div>');
+      chai.expect(document.getElementById('root').innerHTML).to.equal('<div><p>paraTry</p></div>');
     }, 500);
     // XReact.render(app.render(), document.getElementById("root"));
 
